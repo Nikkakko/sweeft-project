ec36395069209402528da504af27222570799650
Nika Kopadze <nikolozkopadze@gmail.com>
2023-03-18 15:09:49 +0400

    fixed axios

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.VITE_API_URL;
+
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
